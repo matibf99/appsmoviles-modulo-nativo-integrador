04ab12c0ae61b86573cbb9fc482bea49672b90d0
matibf99 <matibf99@gmail.com>
2021-11-21 18:03:00 -0300

    Primer commit

---
+++ app/src/main/java/com/appsmoviles/gruposcomunitarios/presentation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.appsmoviles.gruposcomunitarios.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.appsmoviles.gruposcomunitarios.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
