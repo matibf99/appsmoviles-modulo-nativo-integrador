7f448ec482c15da95db78d7b038ba5273efe74d6
Chaza07 <Chazita9@gmail.com>
2021-11-29 17:19:54 -0300

    Guardado de sesion

--- app/src/main/java/com/appsmoviles/gruposcomunitarios/presentation/AuthActivity.kt
+++ app/src/main/java/com/appsmoviles/gruposcomunitarios/presentation/AuthActivity.kt
@@ -1,20 +1,100 @@
 package com.appsmoviles.gruposcomunitarios.presentation
 
+import android.content.ContentProviderClient
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AlertDialog
+import com.appsmoviles.gruposcomunitarios.R
 import com.appsmoviles.gruposcomunitarios.databinding.ActivityAuthBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class AuthActivity : AppCompatActivity() {
+    companion object{
+        private const val RC_SIGN_IN = 120
+    }
 
+    private lateinit var mAuth: FirebaseAuth
     private lateinit var binding: ActivityAuthBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        //Configure Google Sing In
+        // Configure Google Sign In
+
+        binding.googleButton.setOnClickListener {
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                //.requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            googleSignInClient = GoogleSignIn.getClient(this, gso)
+            googleSignInClient.signOut()
+
+            startActivityForResult(googleSignInClient.signInIntent, RC_SIGN_IN)
+        }
+
+        //Setup
         setup()
+        session()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.authLayout.visibility = View.VISIBLE
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val account = task.getResult(ApiException::class.java)
+                if(account != null){
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            //Navegamos a la nueva pantalla
+                            showHome(account.email?: "", ProviderType.GOOGLE)
+                        } else {
+                            showAlert()
+                        }
+                    }
+                }
+            } catch (e: ApiException) {
+                showAlert()
+            }
+
+
+
+        }
+    }
+
+    private fun session() {
+        val prefs = getSharedPreferences(getString(R.string.prefs_files), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider= prefs.getString("provider", null)
+
+        if(email != null && provider != null ){
+            binding.authLayout.visibility = View.INVISIBLE
+            showHome(email, ProviderType.valueOf(provider))
+        }
     }
 
     private fun setup() {
@@ -47,6 +127,11 @@ class AuthActivity : AppCompatActivity() {
                 }
 
             }
+        }
+
+        //Google auth
+        binding.googleButton.setOnClickListener {
+
         }
     }
 
--- app/src/main/java/com/appsmoviles/gruposcomunitarios/presentation/HomeActivity.kt
+++ app/src/main/java/com/appsmoviles/gruposcomunitarios/presentation/HomeActivity.kt
@@ -1,13 +1,18 @@
 package com.appsmoviles.gruposcomunitarios.presentation
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import com.appsmoviles.gruposcomunitarios.R
 import com.appsmoviles.gruposcomunitarios.databinding.ActivityAuthBinding
 import com.appsmoviles.gruposcomunitarios.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 enum class ProviderType {
-    BASIC
+    BASIC,
+    GOOGLE
 }
 
 class HomeActivity : AppCompatActivity() {
@@ -18,5 +23,31 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //
+
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider= bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+        //Guardado de datos
+
+        val prefs = getSharedPreferences(getString(R.string.prefs_files), Context.MODE_PRIVATE).edit()
+        prefs.putString("email",email)
+        prefs.putString("provider", provider)
+        prefs.apply()
+    }
+
+    private fun setup(email: String, provider: String){
+        title= "Inicio"
+        binding.cerrarSesionButton.setOnClickListener {
+            //Borrado de datos
+            val prefs = getSharedPreferences(getString(R.string.prefs_files), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+
     }
 }
